79e6b65385e3ef42a435620ed6cb5246a32406f0
HUUTRUNG\Nguyen Huu Trung <huutrung.mmt@gmail.com>
2020-04-08 10:03:17 +0700

    update(photo): add public url in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
+// vue.config.js
 module.exports = {
-  // Project deployment base
-  // By default we assume your app will be deployed at the root of a domain,
-  // e.g. https://www.my-app.com/
-  // If your app is deployed at a sub-path, you will need to specify that
-  // sub-path here. For example, if your app is deployed at
-  // https://www.foobar.com/my-app/
-  // then change this to '/my-app/'
-  baseUrl: '/photojoiner/',
-
-  // where to output built files
-  outputDir: 'dist',
+  publicPath: 'http://demo.gumiviet.com:8080/photojoiner/',
 }
